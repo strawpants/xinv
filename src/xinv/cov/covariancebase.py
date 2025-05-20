59bdcbfce63ff6f26bcd4a6e7646461e9971fa40
Kiana Karimi <karimis@linux668.itc.utwente.nl>
2025-05-20 21:27:10 +0200

    fix dimension mismatch in covariance matrix handling

--- src/xinv/cov/covariancebase.py
+++ src/xinv/cov/covariancebase.py
@@ -1,10 +1,32 @@
 ## Permissions: See the  xinv  license file https://raw.githubusercontent.com/strawpants/xinv/master/LICENSE
 ## Copyright (c) 2025 Roelof Rietbroek, r.rietbroek@utwente.nl
 
-class CovarianceBase:
-    def __init__(self,cov=None,N=None):
-        self._cov=cov
-        self._N=N
+import xarray as xr
 
-    def decorrelate(self,damat):
-        raise NotImplementedError
+class CovarianceBase:
+    def __init__(self,N=None,cache=False,decorrname="decorrelated"):
+      #  self._cov=None
+        self._N=N
+        self._cache_decorrelate=cache
+        self._decorr=None
+        self._decorrname=decorrname
+
+    def decorrelate(self,damat=None):
+        """Decorrelate the damat using cholesky of the normal matrix"""
+        
+        if self._N is None and damat is None:
+            raise ValueError("Requesting the decorrelated damat requires a valid covariance matrix")
+        elif damat is None:
+            return self._decorr
+
+
+
+        decorr=self._decorrelate_impl(damat)
+        if type(decorr) == xr.DataArray:
+            decorr.name=self._decorrname
+            decorr=decorr.to_dataset()
+
+        if self._cache_decorrelate:
+            self._decorr=decorr
+            
+        return decorr
